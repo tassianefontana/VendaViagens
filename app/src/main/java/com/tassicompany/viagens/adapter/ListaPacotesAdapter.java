261c569e1c075baedb78b5bcef06dc3570df5afd
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-02-18 23:25:10 -0300

    code refactoring

--- app/src/main/java/com/tassicompany/viagens/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/com/tassicompany/viagens/adapter/ListaPacotesAdapter.java
@@ -9,22 +9,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.tassicompany.viagens.R;
 import com.tassicompany.viagens.model.Pacote;
 
-import org.w3c.dom.Text;
-
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class ListaPacotesAdapter extends BaseAdapter {
 
     private final List<Pacote> pacotes;
-    private Context context;
+    private final Context context;
 
-    public ListaPacotesAdapter(List<Pacote> pacotes, Context context){
+    public ListaPacotesAdapter(List<Pacote> pacotes, Context context) {
         this.pacotes = pacotes;
         this.context = context;
     }
@@ -46,26 +47,52 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
     @Override
     public View getView(int posicao, View view, ViewGroup parent) {
-       @SuppressLint("ViewHolder") View viewCriada = LayoutInflater.from(context).
-               inflate(R.layout.item_pacote, parent, false);
+        @SuppressLint("ViewHolder") View viewCriada = LayoutInflater.from(context).
+                inflate(R.layout.item_pacote, parent, false);
 
         Pacote pacote = pacotes.get(posicao);
-        TextView local = viewCriada.findViewById(R.id.item_pacote_cidade);
-        local.setText(pacote.getLocal());
 
+        mostraLocal(viewCriada, pacote);
+        mostraImagem(viewCriada, pacote);
+        mostraQtdDias(viewCriada, pacote);
+        mostraPreco(viewCriada, pacote);
+
+        return viewCriada;
+    }
+
+    private void mostraPreco(View viewCriada, Pacote pacote) {
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        BigDecimal precodoPacote = pacote.getPreco();
+        NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(new Locale("pt", "br"));
+        String moedaBrasileira = formatoBrasileiro
+                .format(precodoPacote)
+                .replace("R$", "R$ ");
+        preco.setText(moedaBrasileira);
+    }
+
+    private void mostraQtdDias(View viewCriada, Pacote pacote) {
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        String diasEmTexto = "";
+        int qtdDias = pacote.getDias();
+
+        if (qtdDias > 1) {
+            diasEmTexto = qtdDias + " dias";
+        } else {
+            diasEmTexto = qtdDias + " dia";
+        }
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(View viewCriada, Pacote pacote) {
         ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
         Resources resources = context.getResources();
         int idDoDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
         Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
         imagem.setImageDrawable(drawableImagemPacote);
+    }
 
-        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
-        dias.setText(pacote.getDias() + " dias");
-
-        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
-        preco.setText(pacote.getPreco().toString());
-
-
-        return viewCriada;
+    private void mostraLocal(View viewCriada, Pacote pacote) {
+        TextView local = viewCriada.findViewById(R.id.item_pacote_cidade);
+        local.setText(pacote.getLocal());
     }
 }
--- app/src/main/java/com/tassicompany/viagens/view/ListaPacotesActivity.java
+++ app/src/main/java/com/tassicompany/viagens/view/ListaPacotesActivity.java
@@ -1,27 +1,32 @@
 package com.tassicompany.viagens.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.tassicompany.viagens.R;
 import com.tassicompany.viagens.adapter.ListaPacotesAdapter;
-import com.tassicompany.viagens.model.Pacote;
 import com.tassicompany.viagens.dao.PacoteDAO;
+import com.tassicompany.viagens.model.Pacote;
 
 import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Pacotes";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        setTitle(TITULO_APPBAR);
+        configuraLista();
+    }
+
+    private void configuraLista() {
         ListView listaPacotes = findViewById(R.id.lista_pacotes_list_view);
         List<Pacote> pacotes = new PacoteDAO().lista();
         listaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
-
-
 }
