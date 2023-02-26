cf70e422a1725b44f703ac833275a51e2eea58ae
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-02-25 23:13:11 -0300

    Create ResumoPacote activity and implement xml

---
+++ app/src/main/java/com/tassicompany/viagens/view/ResumoPagamentoActivity.java
@@ -0,0 +1,16 @@
+package com.tassicompany.viagens.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tassicompany.viagens.R;
+
+public class ResumoPagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pagamento);
+    }
+}
