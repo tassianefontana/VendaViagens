5f4cdce1e680ded56af0d8400ba1d25a26d574eb
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-02-25 21:23:29 -0300

    Implement ActivityPagamento

--- app/src/main/java/com/tassicompany/viagens/PagamentoActivity.java
+++ app/src/main/java/com/tassicompany/viagens/PagamentoActivity.java
@@ -3,12 +3,31 @@ package com.tassicompany.viagens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tassicompany.viagens.model.Pacote;
+import com.tassicompany.viagens.util.MoedaUtil;
+
+import java.math.BigDecimal;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("Sao Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+        mostraPreco(pacoteSaoPaulo);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView tvPreco = findViewById(R.id.activity_pagamento_valor);
+        String moedaBr = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        tvPreco.setText(moedaBr);
     }
 }
--- app/src/main/java/com/tassicompany/viagens/view/ResumoPacoteActivity.java
+++ app/src/main/java/com/tassicompany/viagens/view/ResumoPacoteActivity.java
@@ -1,5 +1,6 @@
 package com.tassicompany.viagens.view;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -7,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.tassicompany.viagens.PagamentoActivity;
 import com.tassicompany.viagens.R;
 import com.tassicompany.viagens.model.Pacote;
 import com.tassicompany.viagens.util.DataUtil;
@@ -33,6 +35,10 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         mostraDias(pacoteSaoPaulo);
         mostraPreco(pacoteSaoPaulo);
         mostraData(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, PagamentoActivity.class);
+        startActivity(intent);
+
     }
 
     private void mostraData(Pacote pacote) {
